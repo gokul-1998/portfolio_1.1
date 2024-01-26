c0e43c815982f9fa2f39956f077236825cb580af
gokul-1998 <gokulakrishnanm1998@gmail.com>
2024-01-26 08:54:26 +0530

    fixed bug

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,13 +2,13 @@
 
 const showMenu = (toggleId, navId) => {
     const toggle = document.getElementById(toggleId),
-        nav = document.getElementById(navId);
-    
-    if (toggle && nav) {
-        toggle.addEventListener("click", () => {
-        nav.classList.toggle("show");
-        });
-    }
-    }
+          nav = document.getElementById(navId)
 
-showMenu("nav-toggle", "nav-menu");
+    if(toggle && nav){
+          toggle.addEventListener('click', () =>{
+                nav.classList.toggle('show')
+          });
+    }
+}
+
+showMenu('nav_toggle','nav_menu')
